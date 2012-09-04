c9710d8b05c72c42e866ef7e0b76f7b79782cab8
Any2Dev <any2dev@gmail.com>
2012-09-04 11:32:59 +0900

    memcpy test

---
+++ MemCpy.cpp
@@ -0,0 +1,56 @@
+// MemCpy.cpp : 콘솔 응용 프로그램에 대한 진입점을 정의합니다.
+//
+
+#include "stdafx.h"
+#include <memory.h>
+
+//-------------------------------------------------------------------------------------------------
+void MemCpyInt()
+{
+	int destSize = 4;
+	int* dest = new int[destSize];
+
+	for ( int i = 0; i < destSize; i++ )
+	{
+		dest[i] = i;
+	}
+
+
+	int srcSize = 4;
+	int* src = new int[srcSize];
+
+	for ( int i = 0; i < srcSize; i++ )
+	{
+		src[i] = i + 10;
+	}
+
+	memcpy_s( dest, destSize, src, destSize );
+
+	int yblack = 0;
+}
+
+//-------------------------------------------------------------------------------------------------
+void MemCpyChar()
+{
+	const int destSize = 9;
+	char dest[destSize] = "destdest";
+
+	const int srcSize = 4;
+	char src[srcSize] = "src";
+
+	memcpy_s( dest, destSize, src, destSize );
+
+	int yblack = 0;
+}
+
+//-------------------------------------------------------------------------------------------------
+int _tmain(int argc, _TCHAR* argv[])
+{
+	MemCpyInt();
+
+	MemCpyChar();
+
+	return 0;
+}
+
+//-------------------------------------------------------------------------------------------------
